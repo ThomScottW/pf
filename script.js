1456bbda9eefab0601c9c34eafb10210631f3f57
Thomas Scott <scotttw@Thomas-MacBook-Air.local>
2023-02-15 01:08:21 -0800

    Fixed typo

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// const proj1Title = document.getElementById("proj1-title");
-// const proj1Description = document.getElementById("proj1-description");
-
-// proj1Title.addEventListener("click", function() {
-//   proj1Title.style.marginBottom = "20px";
-//   proj1Description.classList.remove("hidden");
-// });
 
 
-const projects = documents.getElementsByClassName("projects-col");
+const projects = document.getElementsByClassName("projects-col");
 
 for (let i = 0; i < projects.length; i++)
 {
